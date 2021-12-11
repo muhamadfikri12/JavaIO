e80a0e388c51aae09138ce0b50adf5ce1c0ec1da
Muhamad Fikri <muhamad.fikri@unida.ac.id>
2021-12-11 11:02:41 +0700

    JavaIO

---
+++ src/Angka.java
@@ -0,0 +1,14 @@
+public class Angka {
+	public static void main(String[] args) {
+		int x,y;
+
+		for (x =1; x <= 5; x++) {
+			System.out.println( " " );
+
+			for (y =1; y <= x; y++) {
+				System.out.print(y+" " );
+			}
+		}
+	}
+}
+
---
+++ src/Bintang.java
@@ -0,0 +1,13 @@
+public class Bintang {
+	public static void main(String[] args) {
+		int x,y;
+
+		for (x =1; x <= 5; x++) {
+			System.out.println( " " );
+
+			for (y =1; y <= x; y++) {
+				System.out.print( "*" );
+			}
+		}
+	}
+}
